060e62619c445987be11edb8487ae05e67f9d68e
Alex <alex@my-devs.com>
2014-03-18 19:19:31 -0400

    First commit

---
+++ js/scripts.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+    
+    //Foursquare API credentials 
+    var FQ_CLIENT_ID = 'LSZOGFICOEAAFA5RXPQPHVRIKH0A2KJFCKWX5YCC5QROCBTW';
+    var FQ_CLIENT_SECRET = 'FJF40UWZ022CGGB0KNAS3FMMZY1EZSXOOVEMEWKU00Y1VWNH';
+    
+    //Foursqaure API version
+    var FQ_API_VERSION = '20130815';
+    
+    /**
+     * Foursquare API venues search base URL
+     * More info here: https://developer.foursquare.com/start/search
+     */
+    var FQ_BASE_URL = 'https://developer.foursquare.com/start/search';
+    
+    //Check if location services are available
+    if (navigator.geolocation) {
+        
+        //Determine our location
+        navigator.geolocation.getCurrentPosition(function(position){
+            //Call API
+            $.getJSON(FQ_BASE_URL, {
+                "client_id": FQ_CLIENT_ID,
+                "client_secret": FQ_CLIENT_SECRET,
+                "v": FQ_API_VERSION,
+                "ll": position.coords.latitude+","+position.coords.longitude,
+                "query": ""
+            }, function(response){
+                 $("#content").html(response);
+            });
+            
+        });
+        
+    } else {
+        alert("Cannot determine current location");
+    }
+    
+});
